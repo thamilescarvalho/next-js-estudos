6628b5ebe0eeb9074a6bae793a64ec3f026b951e
Thamiles e Glayce <thamiles.085@gmail.com>
2025-01-14 16:22:31 -0300

    Primeiros Passos

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+/// esse arquivo para manter as fontes que serão usadas em todo o aplicativo.
+
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
